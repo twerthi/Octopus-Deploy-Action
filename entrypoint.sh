23a150d71b920b0d0c3b359bb34f98ebbab358c2
twerthi <twerthi@hotmail.com>
2020-06-17 16:59:52 -0700

    Testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,34 +1,34 @@
 #!/bin/sh -l# Set field seperator
-IFS="="
+#IFS="="
 
 # Get the arguments that were passed
-for var in "$@"
-do
+#for var in "$@"
+#do
     # Split the var
-    read -a argument <<<"$var"
-    echo "$var"
+#    read -a argument <<<"$var"
+#    echo "$var"
     # Assign variables
-    case "${argument[0]}" in
-      "command")
-        command=${argument[1]}
-        ;;
-      "version")
-        version=${argument[1]}
-        ;;
-      "package-id")
-        packageid=${argument[1]}
-        ;;
-      "octopus-server-url")
-        octopusserverurl=${argument[1]}
-        ;;
-      "octopus-apikey")
-        octopusapikey=${argument[1]}
-        ;;
-      "space")
-        spaceName=${argument[1]}
-        ;;
-    esac
+#    case "${argument[0]}" in
+#      "command")
+#        command=${argument[1]}
+#        ;;
+#      "version")
+#        version=${argument[1]}
+#        ;;
+#      "package-id")
+#        packageid=${argument[1]}
+#        ;;
+#      "octopus-server-url")
+#        octopusserverurl=${argument[1]}
+#        ;;
+#      "octopus-apikey")
+#        octopusapikey=${argument[1]}
+#        ;;
+#      "space")
+#        spaceName=${argument[1]}
+#        ;;
+#    esac
 
-done
+#done
 
-echo "The command is $command"
+echo "You have successfully invoked me."
