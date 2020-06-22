0b2150f22f188ad1fa189acb88fdd70d95a6527b
twerthi <twerthi@hotmail.com>
2020-06-22 14:14:48 -0700

    Figuring it out

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,31 +5,6 @@ IFS="="
 # Get the arguments that were passed
 for var in "$@"
 do
-    # Split the var
-    read -a argument <<<"$var"
-    echo "$var"
-    # Assign variables
-    case "${argument[0]}" in
-      "command")
-        command=${argument[1]}
-        ;;
-      "version")
-        version=${argument[1]}
-        ;;
-      "package-id")
-        packageid=${argument[1]}
-        ;;
-      "octopus-server-url")
-        octopusserverurl=${argument[1]}
-        ;;
-      "octopus-apikey")
-        octopusapikey=${argument[1]}
-        ;;
-      "space")
-        spaceName=${argument[1]}
-        ;;
-    esac
-
+  echo "Variable is $var"
 done
 
-echo "Command is: $command "
